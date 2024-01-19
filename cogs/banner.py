0c29ce7e82171aa96bd19fe5fae1b962e4a06977
littxle <stefaniaknico@gmail.com>
2024-01-19 15:26:56 +0100

    parr bug fix und was neues

---
+++ cogs/banner.py
@@ -0,0 +1,31 @@
+import discord
+from discord.commands import slash_command, Option
+from discord.ext import commands
+import littxlecord
+
+class Banner(littxlecord.Cog, emoji="❓"):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @slash_command(
+        name="banner",
+        description="📱〢Zeigt dir dein Banner oder das Banner eines Members an!",
+    )
+    async def banner_user(self, ctx, member: Option(discord.Member, "Das Mitglied, dessen Banner du sehen möchtest.")):
+
+        if member is None:
+            member = ctx.author
+
+        try:
+            banner_user = await self.bot.fetch_user(member.id)
+            embed = discord.Embed(title=f"Banner von {member}", color=0x7289da)
+            embed.set_image(url=banner_user.banner.url)
+
+            await ctx.respond(embed=embed)
+
+        except AttributeError:
+            await ctx.respond(f"{member.mention} hat kein Banner.):", ephemeral=True)
+
+def setup(bot):
+    bot.add_cog(Banner(bot))
