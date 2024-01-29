18abafc4373e49c1ac12058650733654b4ad4eb7
littxle <117511417+littxle@users.noreply.github.com>
2024-01-29 19:02:18 +0100

    bug fix in willkommen und leave und main.py

---
+++ cogs/events/leave.py
@@ -0,0 +1,71 @@
+import discord
+import aiosqlite
+from discord.commands import slash_command, Option
+from discord.ext import commands
+
+class LeaveCard(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.DB = "leave.db"
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        try:
+            async with aiosqlite.connect(self.DB) as db:
+                await db.execute(
+                    """CREATE TABLE IF NOT EXISTS leave (
+                        guild_id INTEGER,
+                        leave_channel INTEGER
+                    )"""
+                )
+        except aiosqlite.Error as e:
+            print(f"Error creating table: {e}")
+
+    @slash_command()
+    @discord.guild_only()
+    async def leave(self, ctx: discord.ApplicationContext, channel: Option(discord.TextChannel)):
+        try:
+            async with aiosqlite.connect(self.DB) as db:
+                await db.execute(
+                    """
+                    INSERT OR REPLACE INTO leave (guild_id, leave_channel) 
+                    VALUES (?, ?)
+                    """,
+                    (ctx.guild.id, channel.id)
+                )
+                await db.commit()
+
+                embed = discord.Embed(
+                    description=f"Leaves channel set to {channel.mention}.",
+                    color=discord.Color.green(),
+                )
+                await ctx.respond(embed=embed, ephemeral=True)
+        except aiosqlite.Error as e:
+            print(f"Error updating database: {e}")
+
+    @commands.Cog.listener()
+    async def on_member_remove(self, member: discord.Member):
+        try:
+            async with aiosqlite.connect(self.DB) as db:
+                cursor = await db.execute(
+                    "SELECT * FROM leave WHERE guild_id = ?", (member.guild.id,)
+                )
+                entry = await cursor.fetchone()
+
+                if entry:
+                    channel_id = entry[1]
+                    channel = self.bot.get_channel(channel_id)
+
+                    embed = discord.Embed(
+                        description=f"Hey {member.mention}, wir bedauern es sehr, dass du uns verlassen hast. Hoffentlich sehen wir uns wieder!",
+                        color=discord.Color.red(),
+                    )
+                    embed.set_author(
+                        icon_url=member.guild.icon.url, name=member.guild.name
+                    )
+                    await channel.send(embed=embed)
+        except aiosqlite.Error as e:
+            print(f"Error querying database: {e}")
+
+def setup(bot):
+    bot.add_cog(LeaveCard(bot))
--- cogs/events/willkommen.py
+++ cogs/events/willkommen.py
@@ -23,7 +23,8 @@ class WelcomeCard(commands.Cog):
     @slash_command()
     @discord.guild_only()
     async def welcome_setup(
-        self, ctx: discord.ApplicationContext, channel: Option(discord.TextChannel), role_name: Option(discord.Role)
+        self, ctx: discord.ApplicationContext, channel: Option(discord.TextChannel), role_name: Option(discord.Role) 
+
     ):
         async with aiosqlite.connect(self.DB) as db:
             cursor = await db.execute(
@@ -73,7 +74,7 @@ class WelcomeCard(commands.Cog):
                         icon_url=member.guild.icon.url, name=member.guild.name
                     )
                     embed.set_image(
-                        url="https://media.discordapp.net/attachments/1190258623511789603/1192734854073299095/dy1QM5M.png?ex=65aa27e5&is=6597b2e5&hm=480032919b01d512a06c30e4319680ef2f0b2dbbb3af1b4047dc7e5a09fc3957&=&format=webp&quality=lossless&width=1246&height=701"
+                        url="https://tse1.mm.bing.net/th?id=OIP.mJ4zvJuNlVIy_ryGKf_YpgHaC9&pid=Api&P=0&h=180"
                     )
 
                     await channel.send(member.mention, embed=embed)
