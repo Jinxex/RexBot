8bfcbc281f120ba410e5ecdb628efda6f412215c
littxle <stefaniaknico@gmail.com>
2024-02-07 14:15:59 +0100

    paar bug fix und add tempvoice

--- cogs/user/botavatar.py
+++ cogs/user/botavatar.py
@@ -1,37 +1,35 @@
 import discord
 from discord.ext import commands
 from discord.commands import slash_command
-import aiohttp
 
 class Botavatar(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.session = aiohttp.ClientSession()
-
 
     @slash_command(description="Change bot's avatar")
-    @discord.guild_only()
-    @commands.cooldown(1, 86400, commands.BucketType.user)
-    
     async def botavatar(
         self, ctx, img: discord.Attachment
     ):
-        if ctx.user_id == 817435791079768105:
-            data = await img.read()
-            try:
-                await ctx.bot.user.edit(avatar=data)
-                embed = discord.Embed(
-                    title="Avatar Changed ✅",
-                    description="Avatar changed successfully.",
-                    color=discord.Color.green()
-                )
-                await ctx.respond(embed=embed)
-            except discord.errors.HTTPException:
-                await ctx.respond("❌ Failed to change avatar.")
+        if ctx.author.id != 817435791079768105:
+            return await ctx.respond("❌ Only the bot owner can change the bot's avatar.", ephemeral=True )
+
+        data = await img.read()
+        try:
+            await ctx.bot.user.edit(avatar=data)
+            embed = discord.Embed(
+                title="Avatar Changed ✅",
+                description="Avatar changed successfully.",
+                color=discord.Color.green()
+            )
+            await ctx.respond(embed=embed)
+        except discord.errors.HTTPException:
+            await ctx.respond("❌ Failed to change avatar.")
 
     @botavatar.error
     async def botavatar_error(self, ctx, error):
-        if isinstance(error, commands.CommandOnCooldown):
+        if isinstance(error, commands.CommandInvokeError):
+            await ctx.respond("❌ Failed to fetch image.")
+        elif isinstance(error, commands.CommandOnCooldown):
             hours = round(error.retry_after / 3600, 1)
             embed = discord.Embed(
                 title="Cooldown ⏳",
