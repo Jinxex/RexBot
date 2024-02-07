da6ef4910bcd4c6a0065cbe0a3607de47395a8cd
littxle <stefaniaknico@gmail.com>
2024-02-07 15:45:23 +0100

    add

--- cogs/user/botavatar.py
+++ cogs/user/botavatar.py
@@ -10,9 +10,7 @@ class Botavatar(commands.Cog):
     async def botavatar(
         self, ctx, img: discord.Attachment
     ):
-        if ctx.author.id != 817435791079768105:
-            return await ctx.respond("❌ Only the bot owner can change the bot's avatar.", ephemeral=True )
-
+        await ctx.defer()
         data = await img.read()
         try:
             await ctx.bot.user.edit(avatar=data)
@@ -25,18 +23,7 @@ class Botavatar(commands.Cog):
         except discord.errors.HTTPException:
             await ctx.respond("❌ Failed to change avatar.")
 
-    @botavatar.error
-    async def botavatar_error(self, ctx, error):
-        if isinstance(error, commands.CommandInvokeError):
-            await ctx.respond("❌ Failed to fetch image.")
-        elif isinstance(error, commands.CommandOnCooldown):
-            hours = round(error.retry_after / 3600, 1)
-            embed = discord.Embed(
-                title="Cooldown ⏳",
-                description=f"This command is on cooldown. Please try again in {hours} hours.",
-                color=discord.Color.red()
-            )
-            await ctx.respond(embed=embed, ephemeral=True)
+
 
 def setup(bot):
     bot.add_cog(Botavatar(bot))
